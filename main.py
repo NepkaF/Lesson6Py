71133815085012219a648be4c72cc78c36f395b9
hepka <hepka2@mail.ru>
2023-05-01 16:10:41 +0300

    commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Первое задание
+First = int(input())
+dif = int(input())
+n=int(input())
+a=[First]
+for i in range(1,n):
+    a.append(a[i-1]+dif)
+for i in range(n):
+    print(a[i])
+#Второе задание
+Min = int(input())
+Max = int(input())
+for i in range(n):
+    if a[i]<=Max and a[i] >= Min :
+        print(a[
+
